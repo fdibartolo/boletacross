2e3455429b10c3c454984f7ea057ed21fdc84160
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2013-09-02 15:02:15 -0300

    move menu icon to its own directive

--- www/app/js/directives.js
+++ www/app/js/directives.js
@@ -6,6 +6,31 @@ angular.module('Prode.directives', ['jqm'])
       AuthenticationService.logout();
     };
   }])
+  .directive('menuIcon', function(){
+      return {
+        restrict: 'A',
+        templateUrl: 'app/partials/menu_icon.html',
+        replace: true
+        // link: function (scope, element, attrs, controller) {
+        //   scope.$watch('shouldLoadMenu', function(shouldLoadMenu) {
+        //     if (shouldLoadMenu) {
+        //       var user = SessionService.getCurrentUser();
+        //       scope.user = user.first_name + ' ' + user.last_name;
+        //       scope.nickname = user.nick_name;
+
+        //       CommunityService.getCommunityStats().then(function(stats) {
+        //         scope.stats = stats;
+        //         SessionService.setCommunityStats(stats);
+        //       });
+        //     } else {
+        //       scope.user = null;
+        //       scope.nickname = null;
+        //       scope.stats = null;
+        //     }
+        //   });
+        // }
+      }
+  })
   .directive('menu', [
     '$rootScope', 'SessionService', 'CommunityService', function($rootScope, SessionService, CommunityService){
       return {
