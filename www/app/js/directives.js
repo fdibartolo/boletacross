1db026c1303b299491776707769816e5dcd30480
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2013-09-02 22:03:41 -0300

    implement pull avatar from backend server

--- www/app/js/directives.js
+++ www/app/js/directives.js
@@ -20,6 +20,7 @@ angular.module('Prode.directives', ['jqm'])
               var user = SessionService.getCurrentUser();
               scope.user = user.first_name + ' ' + user.last_name;
               scope.nickname = user.nick_name;
+              scope.avatar_url = user.image_url;
 
               CommunityService.getCommunityStats().then(function(stats) {
                 scope.stats = stats;
@@ -28,6 +29,7 @@ angular.module('Prode.directives', ['jqm'])
             } else {
               scope.user = null;
               scope.nickname = null;
+              scope.avatar_url = null;
               scope.stats = null;
             }
           });
