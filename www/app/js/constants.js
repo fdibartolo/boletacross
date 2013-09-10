6e8c800a9b66b7f2ab9fdbba6a628d7a6c788373
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2013-09-10 18:23:21 -0300

    redo constants

--- www/app/js/constants.js
+++ www/app/js/constants.js
@@ -1,11 +1,13 @@
 'use strict';
 
+var baseUrl = 'http://0.0.0.0:3000';
+// var baseUrl = 'https://desolate-bayou-3667.herokuapp.com'
+
 angular.module('Prode.constants', ['jqm']).
   value('version', '0.1').
-  value('usersUrl', 'http://0.0.0.0:3000/api/users').
-  // value('usersUrl', 'https://desolate-bayou-3667.herokuapp.com/api/users').
-  value('communityUrl', 'http://0.0.0.0:3000/api/community').
-  value('cardsUrl', 'http://0.0.0.0:3000/api/cards');
+  value('usersUrl', baseUrl + '/api/users').
+  value('communityUrl', baseUrl + '/api/community').
+  value('cardsUrl', baseUrl + '/api/cards');
 
 // angular.module('Prode.constants', ['jqm']).
 //   constant('Settings', {
